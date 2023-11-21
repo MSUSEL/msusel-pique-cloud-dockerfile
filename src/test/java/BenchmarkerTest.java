5b904207145609c90246f3b89cbc25b0bb491d83
Derek Reimanis <derek.reimanis@gmail.com>
2023-11-21 09:18:21 -0700

    changes to script that grabs CWEs from CVEs based on NVD query directly

--- src/test/java/BenchmarkerTest.java
+++ src/test/java/BenchmarkerTest.java
@@ -2,14 +2,20 @@ import calibration.CloudBenchmarker;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import pique.analysis.ITool;
+import pique.evaluation.Project;
+import pique.model.QualityModel;
 import pique.utility.PiqueProperties;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Set;
 
 public class BenchmarkerTest {
+
+    private static CloudBenchmarker benchmarker;
 
 
 
@@ -17,12 +23,22 @@ public class BenchmarkerTest {
 
     }
 
+    @BeforeClass
+    public static void setup(){
+        benchmarker = new CloudBenchmarker();
+    }
+
 
     @Test
     public void testBenchmarkImport(){
-        CloudBenchmarker benchmarker = new CloudBenchmarker();
         Properties prop = PiqueProperties.getProperties();
         Set<Path> dockerImages = benchmarker.getDockerImagesToAnalyze(Paths.get(prop.getProperty("benchmark.repo")));
+
+    }
+
+    public void testAnalyzeProjects(){
+
+       // ArrayList<Project> projects = benchmarker.analyzeProjects(Set<Path> dockerImages, QualityModel qmDescription, Set< ITool > tools)
 
     }
 }
