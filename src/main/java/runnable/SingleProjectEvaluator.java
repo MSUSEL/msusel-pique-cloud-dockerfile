aa2e48795b94d514396def38c54adf5ee470de11
Derek Reimanis <derek.reimanis@gmail.com>
2023-12-14 11:24:02 -0700

    adding and cleaning

--- src/main/java/runnable/SingleProjectEvaluator.java
+++ src/main/java/runnable/SingleProjectEvaluator.java
@@ -67,8 +67,8 @@ public class SingleProjectEvaluator extends ASingleProjectEvaluator {
 
         //ITool gyrpeWrapper = new GrypeWrapper(prop.getProperty("github-token-path"));
         ITool gyrpeWrapper = new GrypeWrapper(prop.getProperty("github-token-path"), prop.getProperty("nvd-api-key-path"));
-        // TODO ITool trivyWrapper = new TrivyWrapper(prop.getProperty("github-token-path"));
-        Set<ITool> tools = Stream.of(gyrpeWrapper).collect(Collectors.toSet());
+        ITool trivyWrapper = new TrivyWrapper(prop.getProperty("github-token-path"));
+        Set<ITool> tools = Stream.of(gyrpeWrapper, trivyWrapper).collect(Collectors.toSet());
 
         for (Path dockerfile : dockerfilesToAnalyze) {
             Path outputPath = runEvaluator(dockerfile, resultsDir, qmLocation, tools);
