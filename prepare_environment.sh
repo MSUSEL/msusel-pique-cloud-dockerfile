4d1a5b9958d04e5f40b3b46f7ca0e3d9bc9c0b17
Ryan <ryan.cummings1@montana.edu>
2024-02-20 15:23:49 -0700

    Version one draft complete

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-########################################
-# Prepares Environment For PIQUE-Cloud
-########################################
+########################################################################
+# Prepares Environment For PIQUE-Cloud and runs PIQUE on sample targets
+########################################################################
 
 WORKDIR="$PWD"
+INPUT_MESSAGE="########################################################\n#   INPUT NEEDED    \n########################################################\n"
 
-# Dependency Check
+# Dependency Checks
 function check_deps() {
     # Check that wget is installed
     printf "Checking dependencies...\n"
     [ -x /usr/bin/wget ] || { printf "ERROR: wget is not installed. Please install wget on your system or perform manual installation of PIQUE-Cloud\n"; exit 1; }
     printf "\twget is installed\n"
 
-    #   TODO Validate access to docker
+    # Check that docker is installed and user is logged in
     ([ -f $HOME/.docker/config.json ] && [ $(grep -c '"auths" {}:' "$HOME/.docker/config.json") -eq 0 ]) \
         || { printf "There may be an issue with your docker configuration or docker may not be installed.\n"; exit 1; }
     printf "\tdocker is installed and you are logged in\n"
@@ -21,19 +22,18 @@ function check_deps() {
     # Check git is installed
     git --version >/dev/null 2>&1 || { printf "git is not installed. Please install git to continue PIQUE-Cloud setup\n"; exit 1; }
     printf "\tgit is installed\n"
-    printf "Dependency check completed successfully\n"
+    printf "Dependency check completed successfully\n\n"
 }
 
+# Check for/create input, output, and subdirectories
 function set_up_directories() {
     printf "Checking directory structure...\n"
     [ -d "$WORKDIR/input/keys" ] || { mkdir -p "$WORKDIR/input/keys"; }
     [ -d "$WORKDIR/output/" ] || { mkdir -p "$WORKDIR/output"; }
-    printf "Directory check/setup complete\n"
+    printf "Directory check/setup complete\n\n"
 }
 
-#   TODO advanced - Automate key creation
-#   IMPORTANT!!! This will overwrite existing key files. Double check user wants to create
-#       new key and has backed up any old keys
+# Checks for existing keys. If they don't exist, prompt user to create them and paste into prompts
 function get_authentication_keys() {
     printf "Checking authentication keys...\n"
 
@@ -41,40 +41,34 @@ function get_authentication_keys() {
     GITHUB_TOKEN_PATH="$WORKDIR/input/keys/github-token.txt"
 
     # Check if keys already exist and provide option to paste in keys
-    [ -f "$NVD_KEY_PATH" ] || { printf "Please generate an NVD API key and enter the key here: "; \
+    # Future Work: Key validation
+    [ -f "$NVD_KEY_PATH" ] || { printf "$INPUT_MESSAGE"; \
+        printf "Please generate an API key with the National Vulnerability Database here https://nvd.nist.gov/developers/request-an-api-key\n"; \
+        printf "Be sure to save it somewhere secure. If you lose the key, you will need to generate a new one\n"; \
+        printf "Once you have retrieved your key, enter the it here: "; \
         read nvd_key; \
         echo "$nvd_key" > $NVD_KEY_PATH; }
     printf "\tNVD Key setup complete\n"
-        # TODO Validate nvd_key exists and is in the correct format
-        #if [ null or misformed ] {
-        # print error and retry - give option to exit
-        #}
-        # printf "nvd-key is %s " "$nvd_key"
-        # printf "\n"
-        # Write key to correct file
 
-    [ -f "$GITHUB_TOKEN_PATH" ] || { printf "Please generate a Github personal access token with ......permissions. Then paste the public key here: "; \
+    # Future Work: Key validation
+    [ -f "$GITHUB_TOKEN_PATH" ] || { printf "$INPUT_MESSAGE"; \
+        printf "Please generate a Github personal access token with ......permissions. Then paste the public key here: "; \
         read github_key; \
         echo "$github_key" > "$GITHUB_TOKEN_PATH"; }
     printf "\tGithub token setup complete\n"
-        # TODO Validate github_key exists and is in the correct format
-        #if [ null or misformed ] {
-        # print error and retry - give option to exit
-        #}
-        # printf "github key is %s " "$github_key"
-        # printf "\n"
-    printf "Authentication setup complete\n"
+    printf "Authentication setup complete\n\n"
 }
 
+# Grabs the sample target file from msusel-pique-cloud-dockerfile github repo
 function generate_sample_target() {
     printf "Checking if target file exists...\n"
     WGET_OPTS="-O $WORKDIR/input/docker-image-target.json"
-    TARGET="https://github.com/MSUSEL/msusel-pique-cloud-dockerfile/blob/master/input/docker-image-target.json"
+    TARGET="https://raw.githubusercontent.com/MSUSEL/msusel-pique-cloud-dockerfile/master/input/docker-image-target.json"
 
     [ -f "$WORKDIR/input/docker-image-target.json" ] || { printf "Getting a sample target file from PIQUE-Cloud\n"; \
         wget $WGET_OPTS $TARGET; \
         printf "Sample target file created\n"; }
-    printf "Target file configured\n"
+    printf "Target file configured\n\n"
 }
 
 function run() {
@@ -83,8 +77,7 @@ function run() {
     get_authentication_keys
     generate_sample_target
 
-    #TODO This docker command is not working in my environment yet. There is likely much more error handling and env validation to do in this file
-    docker run -it --rm -v "/var/run/docker.sock:/var/run/docker.sock:rw" -v $WORKDIR/input:/input -v $WORKDIR/output:/output pique-cloud-dockerfile:latest
+    docker run -it --rm -v "/var/run/docker.sock:/var/run/docker.sock:rw" -v $WORKDIR/input:/input -v $WORKDIR/output:/output msusel/pique-cloud-dockerfile:latest
 }
 run
 
